7a3f6c49d96ac05c9e2d8db68c158a889bb0ab6f
mdemonmia <emonrpi@gmail.com>
2026-04-21 19:49:03 +0600

    add github action for selenium

--- src/main/java/com/example/new_maven/BaseTest.java
+++ src/main/java/com/example/new_maven/BaseTest.java
@@ -1,8 +1,40 @@
+//package com.example.new_maven;
+//
+//import org.openqa.selenium.JavascriptExecutor;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.support.ui.WebDriverWait;
+//import org.testng.annotations.AfterSuite;
+//import org.testng.annotations.BeforeSuite;
+//
+//public class BaseTest {
+//	
+//	protected static WebDriver driver;
+//	protected WebDriverWait wait;
+//	protected JavascriptExecutor js;
+//	
+//	@BeforeSuite
+//	public void setup() {
+//		driver=new ChromeDriver();
+//		driver.manage().window().maximize();
+//	}
+//	
+//	@AfterSuite
+//	public void tearDown() {
+//		if(driver !=null) {
+//			driver.quit();
+//			driver=null;
+//		}
+//	}
+//
+//}
+
 package com.example.new_maven;
 
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;  // ← import যোগ করো
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
@@ -15,16 +47,20 @@ public class BaseTest {
 	
 	@BeforeSuite
 	public void setup() {
-		driver=new ChromeDriver();
+		ChromeOptions options = new ChromeOptions();  // ← যোগ করো
+		options.addArguments("--headless");           // ← যোগ করো
+		options.addArguments("--no-sandbox");         // ← যোগ করো
+		options.addArguments("--disable-dev-shm-usage"); // ← যোগ করো
+		
+		driver = new ChromeDriver(options);  // ← options পাঠাও
 		driver.manage().window().maximize();
 	}
 	
 	@AfterSuite
 	public void tearDown() {
-		if(driver !=null) {
+		if(driver != null) {
 			driver.quit();
-			driver=null;
+			driver = null;
 		}
 	}
-
 }
